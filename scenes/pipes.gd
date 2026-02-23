0942e572b46feaf5415c0ce83f08d38b819d5520
Sizzlingsisig <herniachristianjoseph@gmail.com>
2026-02-24 04:45:28 +0800

    feat: added parallax background, pipes functionality, and basic flappy bird game model

---
+++ scenes/pipes.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+signal point_earned # This is the "shout" the Game script is listening for
+
+var move_speed: float = 200.0
+
+func _process(delta):
+	position.x -= move_speed * delta
+	if position.x < -100:
+		queue_free()
+
+func _on_score_area_body_entered(body):
+	if body.name == "Player":
+		point_earned.emit() # This triggers the _on_pipe_point_earned in Game.gd
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,26 @@ extends CharacterBody2D
 var gravity: float = 1200.0
 var flap_power: float = -400.0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
+	# 1. Apply Gravity
 	velocity.y += gravity * delta
 
+	# 2. Handle Flap (Dash Up)
 	if Input.is_action_just_pressed("flap"):
 		velocity.y = flap_power
+		animated_sprite.play("dash_up")
 
 	move_and_slide()
+	
+	# 3. Handle Transitions (Fall and Idle)
+	# If we are NOT currently playing the dash animation (let it finish or wait until falling)
+	if animated_sprite.animation != "dash_up" or velocity.y > 0:
+		
+		if is_on_floor():
+			# If touching the ground, we are idle
+			animated_sprite.play("idle")
+		elif velocity.y > 0:
+			# If in the air and falling down, play fall animation
+			animated_sprite.play("fall")
