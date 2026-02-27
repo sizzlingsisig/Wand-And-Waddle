83353beeba36420ddec64edbf930a6d714b1d1f2
Sizzlingsisig <herniachristianjoseph@gmail.com>
2026-02-27 21:33:42 +0800

    feat: added global signal bus for game events

--- scripts/pipes.gd
+++ scripts/pipes.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-signal point_earned # This is the "shout" the Game script is listening for
+signal point_earned 
 
 var move_speed: float = 200.0
 
@@ -11,10 +11,9 @@ func _process(delta):
 
 func _on_score_area_body_entered(body):
 	if body.name == "Player":
-		point_earned.emit() # This triggers the _on_pipe_point_earned in Game.gd
+		point_earned.emit()
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		# Instead of reloading the scene, we trigger the function we just made
 		if body.has_method("die"):
 			body.die()
--- scripts/player.gd
+++ scripts/player.gd
@@ -11,7 +11,6 @@ var is_dead: bool = false
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# State 1: The "Get Ready" Hover
 	if not is_started:
 		animated_sprite.play("idle")
 		if Input.is_action_just_pressed("flap"):
@@ -20,35 +19,30 @@ func _physics_process(delta):
 			animated_sprite.play("dash_up")
 			game_started.emit()
 		
-		# The return statement stops the engine from reading the gravity code below
 		return 
 
-	# Apply gravity constantly once the game has started, even if dead
 	velocity.y += gravity * delta
 
-	# State 2: Alive and Active
 	if not is_dead:
 		if Input.is_action_just_pressed("flap"):
 			velocity.y = flap_power
 			animated_sprite.play("dash_up")
 
-		# Handle Animation Transitions
 		if animated_sprite.animation != "dash_up" or velocity.y > 0:
 			if is_on_floor():
 				animated_sprite.play("idle")
 			elif velocity.y > 0:
 				animated_sprite.play("fall")
 				
-	# State 3: Dead and Falling
 	else:
 		animated_sprite.play("explode") 
 
 	move_and_slide()
 
-# A custom function that your pipes will call to trigger the crash
 func die():
 	if not is_dead:
-		print("The Goose has crashed!") # Watch for this in the Output window!
+		print("The Goose has crashed!") 
 		is_dead = true
 		velocity.y = flap_power / 1.5 
+		GameEvents.emit_wizard_died()
 		player_died.emit()
